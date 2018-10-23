44c8cedabeaff125f205ce8249a10aada7a08d17
tishayaem <kostiantynkalinin@gmail.com>
2018-10-23 22:48:16 +0100

    Create `Order` component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,14 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="top">
-        <h1>Catch of the day</h1>
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">Of</span>
+            <span className="the">The</span>
+          </span>
+          day
+        </h1>
 
         <h3 className="tagline">
           <span>Fresh daily</span>
