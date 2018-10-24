52bddb139f921b3519a9b87625a106ae22173de8
tishayaem <kostiantynkalinin@gmail.com>
2018-10-24 22:14:52 +0100

    Open route with restaurant name when we submit the form

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import '../css/style.css'
+import '../css/style.css';
 import { getFunName } from '../helpers';
 
 export default class Restaurant extends React.Component {
-    render() {
-        return (
-            <form className="store-selector">
-                <h2>Enter restaurant name</h2>
-                <input type="text" placeholder="Store Name" defaultValue={getFunName()}/>
-                <button type="submit">Visit Restaurant</button>
-            </form>
-        )
-    }
+  myInput = React.createRef();
+  goToRestaurant = event => {
+    event.preventDefault();
+    const reastaurantName = this.myInput.value.value;
+    this.props.history.push(`/store/${reastaurantName}`);
+  };
+  render() {
+    return (
+      <form onSubmit={this.goToRestaurant} className="store-selector">
+        <h2>Enter restaurant name</h2>
+        <input
+          ref={this.myInput}
+          type="text"
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Restaurant</button>
+      </form>
+    );
+  }
 }
